dbd0fb5a173b9df7363ba26a17eb7218b9213eca
Ritu Agrawal <rituagrawal@Ritus-MacBook-Pro.local>
2023-07-26 11:40:45 -0500

    initial commit

---
+++ src/main/java/com/springbootModules/learning/CaffineCacheDemo/CaffineCacheDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springbootModules.learning.CaffineCacheDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffineCacheDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffineCacheDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootModules/learning/CaffineCacheDemo/CaffineCacheDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootModules.learning.CaffineCacheDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffineCacheDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
